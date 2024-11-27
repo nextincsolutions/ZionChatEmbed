43a7f37669e44a0862602fdde0456fadf38080f0
Anderson Augusto <andersonaugusto@MacBook-Pro-de-Anderson.local>
2024-11-27 09:25:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- markdownlint-disable MD030 -->
+
+# Zion chat Embed
+
+Javascript library to display Zion chatbot on your website
+
+![Flowise](https://github.com/FlowiseAI/FlowiseChatEmbed/blob/main/images/ChatEmbed.gif?raw=true)
---
+++ src/register.tsx
@@ -0,0 +1,11 @@
+import { customElement } from 'solid-element';
+import { defaultBotProps } from './constants';
+import { Bubble } from './features/bubble';
+import { Full } from './features/full';
+
+export const registerWebComponents = () => {
+  if (typeof window === 'undefined') return;
+  // @ts-expect-error element incorect type
+  customElement('zion-fullchatbot', defaultBotProps, Full);
+  customElement('zion-chatbot', defaultBotProps, Bubble);
+};
